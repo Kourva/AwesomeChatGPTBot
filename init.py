9cad1a2e7c84a9d0f4bcde4cacdee824af9e818f
Kourva <118578799+Kourva@users.noreply.github.com>
2023-08-28 08:41:22 -0400

    Create init.py

---
+++ init.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import utils
+import telebot
+import requests
+from telebot import types, util
+
+# Connect to bot
+# Token placed in utils.py file. You can change it with your token
+GPTbot = telebot.TeleBot(utils.Token)
+print(f"The Bot is online (id: {GPTbot.get_me().id}) \33[0;31m[DEVELOPER MODE]\33[m...")
+
+# Set bot commands
+print("[!] Configuring bot commands...")
+GPTbot.set_my_commands(
+    commands=[
+        types.BotCommand(
+            command="start",
+            description="Start the bot"
+        ),
+        types.BotCommand(
+            command="history",
+            description="Get your chat history"
+        ),
+        types.BotCommand(
+            command="reset",
+            description="Reset you chat history"
+        )
+    ]
+)
+print("[!] Commands successfully configured.\n[!] Run main.py")
