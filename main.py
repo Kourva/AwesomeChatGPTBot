f413246f49bd23e1633e74cc2f5a7fd01aa87396
Kourva <118578799+Kourva@users.noreply.github.com>
2023-08-28 08:40:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,16 @@
 # Source code: https://github.com/Kourva/AwesomeChatGPTBot
 
 
-# Libraries
-import telebot                        # Bot API Library
-import requests                       # Internet requests
-import utils                          # Bot Utilities
-import json                           # Json function
-import os                             # OS functions
-from telebot import types, util       # TeleBot utilities
-from telebot.util import quick_markup # Markup generator
-from utils import ChatGPT_Function    # ChatGPT function
+# Resources
+import telebot
+import requests
+import utils
+import json
+import os
+from datetime import datetime
+from telebot import types, util
+from telebot.util import quick_markup
+from utils import ChatGPT_Function
 
 
 # Connect to bot
@@ -25,219 +26,132 @@ print(f"The Bot is online (id: {GPTbot.get_me().id})...")
 
 
 # Start message handler
-@GPTbot.message_handler(commands=["start", "restart"])
-def start_command_handler(message: object) -> None:
+@GPTbot.message_handler(commands=["start"])
+def start_command_handler(message: callable) -> None:
     """
-    Function   to   handle /start  & /restart  command
-    Creates account for user and sends welcome message
+    Function to handle /start command
+
+    Parameter: 
+        Message object
+
+    Returns: 
+        None
     """
 
-    # Search for user account that is exist or not
-    if (ufile := f"{message.from_user.id}") in os.listdir("Accounts/"):
+    # User ID
+    user_id = message.from_user.id
+
+    # Check if the user account exists
+    if os.path.exists(f"Accounts/{user_id}"):
             
-        # Send welcome back message to user
-        GPTbot.send_chat_action(
-            chat_id=message.chat.id, 
-            action="typing"
-        )
+        # Send welcome back message to existing user
+        GPTbot.send_chat_action(chat_id=message.chat.id, action="typing")
         GPTbot.reply_to(
             message=message,
-            text=f"Welcome Back {message.from_user.first_name}.\nLet's chat! (Use /settings for settings).",
+            text=f"Welcome Back {message.from_user.first_name}.\nLet's chat!\n\nCommands usage:\n/history: Get history in json format.\n/reset: Reset the chat",
         )
 
-    # Continue if user is new member and create account for user
     else:
 
-        # Send welcome message to new user
-        GPTbot.send_chat_action(
-            chat_id=message.chat.id, 
-            action="typing"
-        )
+        # Create user account for new member
+        os.mkdir(f"Accounts/{user_id}")
+        with open(f'Accounts/{user_id}/history.json', 'w') as file:
+            json.dump([], file)
+
+        # Send welcome back message to existing user
+        GPTbot.send_chat_action(chat_id=message.chat.id, action="typing")
         GPTbot.reply_to(
             message=message,
-            text=f"Welcome Dear {message.from_user.first_name}.\nLet's chat! (Use /settings for settings).",
-   
+            text=f"Welcome Dear {message.from_user.first_name}.\nLet's chat!\n\nCommands usage:\n/history: Get history in json format.\n/reset: Reset the chat",
         )
 
-        # Initialize user account's files
-        os.mkdir(f"Accounts/{ufile}")                   # User account
-        open(f"Accounts/{ufile}/chatgpt", "w").close()  # GPT history
 
-
-# Settings command handler
-@GPTbot.message_handler(commands=["settings"])
-def gpt_command_handler(message: object) -> None:
+# History command handler
+@GPTbot.message_handler(commands=["history"])
+def gpt_command_handler(message: callable) -> None:
     """
-    Function       to       handle      settings       command
-    Show user chatGPT account information or renew chat status
+    Function to handle /history command
+
+    Parameter: 
+        Message object
+
+    Returns: 
+        None
     """
 
-    # Make reply markup for settings
-    Markups = quick_markup(
-        {
-            "Renew Chat": {
-                "callback_data": f"ReNewGpt_{message.from_user.id}"
-            },
-            "Account Info": {
-                "callback_data": f"InfoGpt_{message.from_user.id}"
-            },
-            "Close": {
-                "callback_data": "Close"
-            },
-        },
-        row_width=2,
-    )
+    # User ID
+    user_id = message.from_user.id
 
-    # Send GPT menu to user
-    GPTbot.send_chat_action(
-        chat_id=message.chat.id,
-        action="typing"
-    )
-    GPTbot.reply_to(
+    # Send Wait prompt 
+    GPTbot.send_chat_action(chat_id=message.chat.id, action="typing")
+    prompt = GPTbot.reply_to(
         message=message,
-        text="Welcome to ChatGPT settings!",
-        reply_markup=Markups
+        text="Sending your chat history..."
+    )
+
+    # Send user's history
+    with open(f"Accounts/{user_id}/history.json", "rb") as file:
+    
+        GPTbot.send_chat_action(chat_id=message.chat.id, action="upload_document")
+        GPTbot.send_document(
+            chat_id=message.chat.id,
+            document=file,
+            caption=f"GPT history. {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
+            reply_to_message_id=prompt.message_id,
+            visible_file_name="GPT History"
+        )
+
+    # Edit previous prompt
+    prompt = GPTbot.edit_message_text(
+        chat_id=message.chat.id,
+        message_id=prompt.message_id,
+        text="Here is your chat history."
     )
 
 
-
-# Callback query handler for buttons used in settings
-@GPTbot.callback_query_handler(func=lambda call: True)
-def callback_query(call: object) -> None:
+# Reset command handler
+@GPTbot.message_handler(commands=["reset"])
+def gpt_command_handler(message: callable) -> None:
     """
-    This function will handle the inline keyboards callback
-    Show    results   and    Answer  the  callback  queries
-    """
-    
-    # Initialize the IDs (User ID), (Chat ID), (Message ID)
-    try:uid = call.from_user.id 
-    except:pass
+    Function to handle /history command
 
-    try:cid = call.message.chat.id
-    except:pass
+    Parameter: 
+        Message object
 
-    try:mid = call.message.message_id
-    except:pass
-
-    
-    # Callback handler for chatGPT history renewing
-    if call.data.startswith("ReNewGpt_"):
-
-        # Error handling
-        try:
-
-            # Get user id from argument
-            user_id = call.data.split("_")[1]
-
-            # Delete chat history
-            with open(f"Accounts/{user_id}/chatgpt", "w") as d:
-                d.write("")
-
-            # Answer query
-            GPTbot.answer_callback_query(
-                call.id,
-                "Your Chat History Deleted!",
-                show_alert=True
-            )
-            # Delete the message (Close)
-            GPTbot.delete_message(
-                chat_id=cid, 
-                message_id=mid
-            )
-
-        # Show error message if operation failed
-        except:
-
-            # Show error message
-            GPTbot.answer_callback_query(
-                call.id,
-                "Could not do this operation :(",
-            )
-
-
-    # Callback handler for Gpt account information
-    elif call.data.startswith("InfoGpt_"):
-
-        # Error handling
-        try:
-
-            # Get user id
-            user_id = call.data.split("_")[1]
-
-            # Get gpt info
-            with open(f"Accounts/{user_id}/chatgpt", "r") as d:
-                
-                # If history file is empty
-                if (datas:=d.read()) == "":
-                    
-                    # Show message
-                    GPTbot.answer_callback_query(
-                        call.id,
-                        "You don't have any chat history yet :("
-                    )
-                
-                # Continue if there is valid config
-                else:
-                    
-                    # Fetch chat id and last message id
-                    chat_id = datas.split("@")[0].strip()
-                    mseg_id = datas.split("@")[1].strip()
-                    
-                    # Show message
-                    GPTbot.answer_callback_query(
-                        call.id,
-                        (
-                            f"▋Conversation ID:\n{chat_id}\n\n"
-                            f"▋Last Message ID:\n{mseg_id}"
-                        ),
-                        show_alert=True
-                    )
-
-        # Show error message if operation failed
-        except:
-
-            # Show error message
-            GPTbot.answer_callback_query(
-                call.id,
-                "Could not do this operation :("
-            )
-
-
-    # Callback handler for Close option
-    elif call.data == "Close":
-
-
-        # Error handling
-        try:
-            
-            # Delete the message (Close)
-            GPTbot.delete_message(
-                chat_id=cid, 
-                message_id=mid
-            )
-
-        except:
-
-            # Show error message
-            GPTbot.answer_callback_query(
-                call.id,
-                "Could not do this operation for now :("
-            )
-
-
-# Inline      handler   for      code      command
-# Don't forget to enable your bot inline mode from
-# your         bot      setting    in   @BotFather 
-@GPTbot.inline_handler(lambda query: query.query == "gpt")
-def query_text(inline_query: object) -> None:
-    """
-    Function to handle   inline gpt prompts
-    Shows settings for gpt account for user
+    Returns: 
+        None
     """
 
-    # Error handling
+    # User ID
+    user_id = message.from_user.id
+
+    # Clear history
+    with open(f'Accounts/{user_id}/history.json', 'w') as file:
+        json.dump([], file)
+
+    # Send message to user
+    GPTbot.send_chat_action(chat_id=message.chat.id, action="typing")
+    prompt = GPTbot.reply_to(
+        message=message,
+        text="Your history cleared."
+    )
+
+
+# Inline handler for GPT prompts
+# Don't forget to enable inline mode from @BotFather 
+@GPTbot.inline_handler(lambda query: query.query == "prompt")
+def query_text(inline_query: callable) -> None:
+    """
+    Function to handle prompt inline
+
+    Parameter: 
+        Inline Query Object
+
+    Returns: 
+        None
+    """
+
     try:
-
         # Create results list
         results = []
 
@@ -263,35 +177,35 @@ def query_text(inline_query: object) -> None:
             inline_query.id, results
         )
 
-    # Pass errors (log)
+    # Pass errors
     except Exception as e:
-    	print(e)
+        print(e)
 
 
-# Handle user messages
-@GPTbot.message_handler(func=lambda message: True)
-def handle_messages(message):
+# Handle text messages
+@GPTbot.message_handler(content_types=["text"])
+def handle_messages(message: callable) -> None:
     """
-    Function     to handle  all  messages  in  bot   (except / commands )
-    This     will    handle   user  messages      with        ChatGPT-3.5
+    Function to handle all text messages
+     
+    Parameter: 
+        Message object
+
+    Returns: 
+        None
     """
 
-    # Avoid        making       mess   for    replying    in       groups
-    # Let's say  I   replied message  to bot  in group  and bot  answered
-    # If you reply  to my  message bot will also  answer to  your message
-    # Because replies are connected, this statement is for  avoiding this
-    # Issue    in     groups.  (  Or     maybe     private        chat  )
+    # User ID
+    user_id = message.from_user.id
+
+    # Gets user input
+    user_prompt = message.text.strip()
+
+    # Avoid making mess for replying in groups
     if message.reply_to_message and message.reply_to_message.from_user.id == GPTbot.get_me().id:
 
-        # Get user input
-        user_input = message.text.strip()
-
         # Send waiting message
-        # Note some characters escaped for parse mode
-        GPTbot.send_chat_action(
-            chat_id=message.chat.id,
-            action="typing"
-        )
+        GPTbot.send_chat_action(chat_id=message.chat.id, action="typing")
         gpt_prompt = GPTbot.reply_to(
             message=message,
             text="Bot is thinking\.\.\.",
@@ -299,7 +213,7 @@ def handle_messages(message):
         ) 
 
         # Fetch results from function
-        result = ChatGPT_Function(message.from_user.id, user_input)
+        result = ChatGPT_Function(user_id, user_prompt)
 
         # Edit waiting message and answer user message
         GPTbot.edit_message_text(
@@ -310,17 +224,11 @@ def handle_messages(message):
         )
 
 
-    # If chat mode is private (chat in bot)
+    # Handle private chat
     elif utils.force_private(message):
 
-        # Gets user input
-        user_input = message.text.strip()
-
         # Send waiting message
-        GPTbot.send_chat_action(
-            chat_id=message.chat.id,
-            action="typing"
-        )
+        GPTbot.send_chat_action(chat_id=message.chat.id, action="typing")
         gpt_prompt = GPTbot.reply_to(
             message=message,
             text="Bot is thinking\.\.\.",
@@ -328,7 +236,7 @@ def handle_messages(message):
         )
 
         # Fetch results from function
-        result = ChatGPT_Function(message.from_user.id, user_input)
+        result = ChatGPT_Function(user_id, user_prompt)
 
         # Answer user message
         GPTbot.edit_message_text(
@@ -339,18 +247,9 @@ def handle_messages(message):
         )
 
 
-# Connect to  bot in  infinite polling mode
-# Make   bot  connection       non     stop
-# Skip      old    messages,  don't  update
+# Connect to bot
 if __name__ == "__main__":
-
-    # Error handling 
     try:
-        GPTbot.infinity_polling(
-            skip_pending=True, 
-            none_stop=True,
-        )
-
-    # Except any error
+        GPTbot.infinity_polling(skip_pending=True, none_stop=True)
     except: 
         print("Lost connection!")
