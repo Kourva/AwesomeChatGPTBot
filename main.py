c9b4ecbd0ba8f0da53aa89b9bdfc608d028d1de3
козёл <118578799+kozyol@users.noreply.github.com>
2024-07-12 08:00:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # FreeChatGPT telegram bot
-# Developed by Kourva
-# Source code: https://github.com/Kourva/AwesomeChatGPTBot
+# Developed by kozyol
+# Source code: https://github.com/kozyol/AwesomeChatGPTBot
 
 # Standard library imports
 import json
